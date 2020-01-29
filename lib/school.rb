5193fe5feea65311af499704b9ffcc96125d84b6
alexandre jamarco <alexandre.j@gmail.com>
2020-01-29 16:56:21 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require "pry"
+class School
+    attr_accessor :roster
+    def initialize(name)
+        @name = name
+        @roster = {}   
+    end
+
+    def add_student(name, grade)
+        if self.roster[grade]
+            self.roster[grade].push(name)
+        else
+            self.roster[grade] = []
+            self.roster[grade].push(name)
+        end
+    end
+
+    def grade(grade_to_return)
+        self.roster[grade_to_return]
+    end
+
+    def sort
+        self.roster.each_key do |key|
+            self.roster[key].sort!
+        end
+    end
+
+end
+
+  
