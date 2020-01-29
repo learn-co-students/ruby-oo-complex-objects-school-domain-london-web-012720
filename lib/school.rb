4f3fc56dbd623cce9207a3b319f2945330bdecd0
Bryn <thenazboz@gmail.com>
2020-01-29 10:56:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+    attr_accessor :roster, :student, :grade
+
+    def initialize(sn)
+        @sn = sn
+        @roster = {}
+    end
+
+    def add_student(student,grade)
+        if roster.key? (grade)
+            roster[grade] << student
+        else
+            roster[grade] = [student]
+        end
+    end
+
+        def grade(g)
+        return roster[g]
+        end
+
+        def sort
+            arr = {}
+            roster.each do |k,v| 
+                arr[k] = v.sort  
+             end
+            arr
+        end
+end
